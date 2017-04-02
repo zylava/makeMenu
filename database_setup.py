4b77542fb81cabe2f6c3299555949374b8fd9f61
zylava <zylava@hotmail.com>
2017-04-01 20:16:58 -0700

    same ingredients can appear in different recipes now

--- database_setup.py
+++ database_setup.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ingredient_catalog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///recipe_catalog.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
@@ -22,29 +22,15 @@ class Description(db.Model):
                            backref=db.backref('description')
                               )
 
-	@property
-	def serialize(self):
-		return {
-			'description': self.description,
-			'recipe_name': self.recipe_name
-		}
-
 class Ingredient(db.Model):
     __tablename__ = 'ingredient'
-
-    name = db.Column(db.String(80), primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(80))
     recipe_name = db.Column(db.String, db.ForeignKey('recipe.name'))
 
     recipe = db.relationship(
                            'Recipe',
                            backref=db.backref('ingredients')
                               )
-
-    @property
-    def serialize(self):
-        return {
-                'name': self.name,
-                'recipe_name': self.recipe_name
-               }
 
 db.create_all()
